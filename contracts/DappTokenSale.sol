c5dafbfb1691f672ea1eeb5491ea571fd26beb71
iveltondequeiroz <iveltondequeiroz@gmail.com>
2018-05-21 17:50:38 -0400

    buy tokens

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -3,49 +3,37 @@ pragma solidity ^0.4.2;
 import "./DappToken.sol";
 
 contract DappTokenSale {
+    address admin;
+    DappToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
 
-  address admin;
-  DappToken public tokenContract;
-  uint256 public tokenPrice;
-  uint256 public tokenSold;
+    event Sell(address _buyer, uint256 _amount);
 
-  event Sell(address _buyer, uint256 _amount);
+    function DappTokenSale(DappToken _tokenContract, uint256 _tokenPrice) public {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
 
-  constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
-    // assign admin
-    admin = msg.sender;
-    // assign token Contract
-    tokenContract = _tokenContract;
-    // token Price
-    tokenPrice = _tokenPrice;
-  }
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
 
-  // multiply
-  function multiply(uint x, uint y) internal pure returns (uint z) {
-    require(y == 0 || (z = x * y) / y == x);
-  }
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
-  function buyTokens(uint256 _numberOfTokens) public payable {
-    // require that value is equal to tokens
-     require(msg.value == multiply(_numberOfTokens, tokenPrice));
-    // require that contract has enough tokens
-    require(tokenContract.balanceOf(this) >= _numberOfTokens);
-    // require that a transfer is successful
-    require(tokenContract.transfer(msg.sender, _numberOfTokens));
-    // keep track of number of tokens sold
-    tokenSold += _numberOfTokens; 
-    // trigger Sell Event
-    emit Sell(msg.sender, _numberOfTokens);
-  }
+        tokensSold += _numberOfTokens;
 
-  function endSale() public {
-    // require admin
-    require(msg.sender == admin);
-    // transfer remaning tokens to admin
-    require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
-    // destroy cointract
-    selfdestruct(admin);  
-  }
+        Sell(msg.sender, _numberOfTokens);
+    }
 
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
 
+        selfdestruct(admin);
+    }
 }
